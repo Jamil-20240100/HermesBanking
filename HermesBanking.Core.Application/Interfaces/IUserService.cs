efaf048c24e3d080e09a54d1afe387c71052692f
Amaury Daniel Romero Gonzalez <2240198@itla.edu.do>
2025-07-14 21:13:22 -0400

    Transacion de cashierService y TransactionService y UserService a application

---
+++ HermesBanking.Core.Application/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using HermesBanking.Core.Application.DTOs.User;
+
+namespace HermesBanking.Core.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDto?> GetUserByIdAsync(string userId);
+        Task<string?> GetUserEmailAsync(string userId);
+        Task<string> GetUserFullNameAsync(string userId);
+    }
+}
---
+++ HermesBanking.Infrastructure.Identity/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using HermesBanking.Core.Application.DTOs.User;
+using HermesBanking.Core.Application.Interfaces;
+using HermesBanking.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace HermesBanking.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserService(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<UserDto?> GetUserByIdAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            if (user == null) return null;
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            return new UserDto
+            {
+                Id = user.Id,
+                UserId = user.Id,
+                UserName = user.UserName ?? "",
+                Name = user.Name ?? "",
+                LastName = user.LastName ?? "",
+                Email = user.Email ?? "",
+                Role = roles.FirstOrDefault() ?? "Unknown",
+                IsActive = user.IsActive,
+                isVerified = user.EmailConfirmed,
+                InitialAmount = null
+            };
+        }
+
+        public async Task<string?> GetUserEmailAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            return user?.Email;
+        }
+
+        public async Task<string> GetUserFullNameAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            return user == null ? "" : $"{user.Name} {user.LastName}";
+        }
+    }
+}
