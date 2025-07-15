7e345348afbce113df97d23b5e18becf32dff876
Amaury Daniel Romero Gonzalez <2240198@itla.edu.do>
2025-07-14 20:17:29 -0400

    Implementacion de dtos en el cajero

---
+++ HermesBanking.Core.Application/DTOs/Cashier/CreditCardPaymentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HermesBanking.Core.Application.DTOs.Cashier
+{
+    public class CreditCardPaymentDto
+    {
+        public string AccountNumber { get; set; } = null!;
+        public string CardNumber { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public string CashierId { get; set; } = null!;
+    }
+}
---
+++ HermesBanking.Core.Application/Mappings/DTOsAndViewModels/CashierDtoMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HermesBanking.Core.Application.DTOs.Cashier;
+using HermesBanking.Core.Application.ViewModels.Cashier;
+
+namespace HermesBanking.Core.Application.Mappings.DTOsAndViewModels
+{
+    public class CashierDtoMappingProfile : Profile
+    {
+        public CashierDtoMappingProfile()
+        {
+            CreateMap<DepositViewModel, DepositDto>().ReverseMap();
+            CreateMap<WithdrawViewModel, WithdrawDto>().ReverseMap();
+            CreateMap<ThirdPartyTransferViewModel, ThirdPartyTransferDto>().ReverseMap();
+            CreateMap<PagoPrestamoViewModel, LoanPaymentDto>().ReverseMap();
+            CreateMap<PagoTarjetaCreditoViewModel, CreditCardPaymentDto>().ReverseMap();
+
+            // Confirmaciones
+            CreateMap<ConfirmDepositViewModel, DepositDto>();
+            CreateMap<ConfirmWithdrawViewModel, WithdrawDto>();
+            CreateMap<ConfirmThirdPartyTransferViewModel, ThirdPartyTransferDto>();
+            CreateMap<ConfirmPagoPrestamoViewModel, LoanPaymentDto>();
+            CreateMap<ConfirmPagoTarjetaCreditoViewModel, CreditCardPaymentDto>();
+        }
+    }
+}
