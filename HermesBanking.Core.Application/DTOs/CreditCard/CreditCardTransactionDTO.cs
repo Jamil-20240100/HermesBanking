68a3d49a028731dba568dfebdac67c3fe57d0e25
Amaury Daniel Romero Gonzalez <2240198@itla.edu.do>
2025-07-21 13:58:34 -0400

    Bla

---
+++ HermesBanking.Core.Application/DTOs/CreditCard/CreditCardTransactionDTO.cs
@@ -0,0 +1,9 @@
+﻿public class CreditCardTransactionDTO
+{
+    public int TransactionId { get; set; }
+    public string Type { get; set; } // Tipo de transacción
+    public decimal Amount { get; set; } // Monto de la transacción
+    public DateTime Date { get; set; } // Fecha de la transacción
+    public string Origin { get; set; } // Origen de la transacción
+    public string Beneficiary { get; set; } // Beneficiario de la transacción
+}
